6519c6b49979ead7dfe86dd761bbe036f382dfbc
Abdullah Harits <38558796+Harits19@users.noreply.github.com>
2022-05-03 10:48:31 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:init_flutter/bloc/home_bloc/home_bloc_cubit.dart';
+import 'package:init_flutter/konstans/konstans.dart';
+import 'package:init_flutter/utils/app_bloc_observer.dart';
+import 'package:init_flutter/utils/get_it_config.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:init_flutter/utils/helper.dart';
+
+void main() async {
+  await setupDepedencyInjection();
+  BlocOverrides.runZoned(
+    () => runApp(DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const MyApp(),
+    )),
+    blocObserver: AppBlocObserver(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => HomeBlocCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        useInheritedMediaQuery: true,
+        theme: ThemeData(
+          primarySwatch: Helper.func.createMaterialColor(KColor.baliHai),
+          splashColor: KColor.blumine.withOpacity(0.4),
+          scaffoldBackgroundColor: Colors.white,
+          // textTheme: newTextTheme,
+          // buttonTheme: ButtonThemeData(),
+          outlinedButtonTheme: OutlinedButtonThemeData(
+            style: KButton.outlinedStyle,
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: KButton.elevatedStyle,
+          ),
+          inputDecorationTheme: KDecor.input,
+        ),
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        home: Scaffold(
+          body: SafeArea(
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: null,
+                  child: Text("ElevatedButton Disable"),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("ElevatedButton Disable"),
+                ),
+                OutlinedButton(
+                  onPressed: null,
+                  child: Text("OutlinedButton Disable"),
+                ),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Text("OutlinedButton Disable"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/gift/gift_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:init_flutter/utils/helper.dart';
+
+class GiftDetailPage extends StatelessWidget {
+  const GiftDetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("GiftDetailPage"),
+      ),
+    );
+  }
+}
